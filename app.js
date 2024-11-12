b6c79333f41d18c2c36f2394890aa907f7673a6d
GabbsDutra <gakillglikerr@gmail.com>
2024-11-11 23:45:48 -0400

    final

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const itemsRoutes = require('./routes/itemsRoutes');
 const app = express();
 const PORT = 3000;
 
-// Middleware
+
 app.use(logger);
 
-// Rotas
+
 app.get('/', (req, res) => res.send('Bem-vindo ao sistema!'));
 app.use('/items', itemsRoutes);
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+
+
+
